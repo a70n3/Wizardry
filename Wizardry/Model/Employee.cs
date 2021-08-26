319ff612257241122b4260f764250fb3d039e9fe
a70n3 <prohibitous@gmail.com>
2021-08-26 23:12:59 +0800

    Add project files.

---
+++ Wizardry/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wizardry.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        public string Street { get; set; }
+        public string Municipality { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
